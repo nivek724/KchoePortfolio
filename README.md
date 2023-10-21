23ff769cb058db1172f4ef5c073293c9a464bc1a
nivek724 <kevin.nivek.choe@gmail.com>
2023-10-21 13:07:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+    
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com?ref=chronark.com), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+If you'd like anymore information about me check out the Website link on this github page. <a href="github.com/nivek724/KchoePortfolio">KchoePortfolio</a>
---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import '../global.css';
+import { Inter } from 'next/font/google';
+import LocalFont from 'next/font/local';
+import { Metadata } from 'next';
+import { Analytics } from './components/analytics';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Kchoe Portfolio',
+    template: '%s | Kchoe Portfolio',
+  },
+  description: 'Software engineer using React and Typescript',
+  openGraph: {
+    title: 'Kchoe Portfolio',
+    description: 'Software engineer using React and Typescript',
+    url: '', //fill later
+    siteName: 'Kchoe Portfolio',
+    images: [
+      {
+        url: 'localhost:3000/favicon.ico', //fill later
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
+};
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const calSans = LocalFont({
+  src: '../public/fonts/CalSans-SemiBold.ttf',
+  variable: '--font-calsans',
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' className={[inter.variable, calSans.variable].join(' ')}>
+      <head>
+        <Analytics />
+      </head>
+      <body className={`bg-black ${process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
